95b24a8e53ec874816e9108679d5674baadc121f
Tobias Ottenweller <tobias.ottenweller@intive-kupferwerk.com>
2018-03-25 17:03:15 +0200

    Basic project setup

---
+++ RealWorldReSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealWorldReSwift
+//
+//  Created by Tobias Ottenweller on 25.03.18.
+//  Copyright © 2018 Tobias Ottenweller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
